e3b35c3aee03e6c05af1e43cda27b216af1bdc8d
LeoGamerMan <mirzajanzadeh.leo@gmail.com>
2026-01-19 10:17:14 +0100

    Init

---
+++ src/main/java/com/grupp5/sf2api/Sf2ApiApplication.java
@@ -0,0 +1,13 @@
+package com.grupp5.sf2api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sf2ApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Sf2ApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/grupp5/sf2api/Sf2ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupp5.sf2api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sf2ApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
