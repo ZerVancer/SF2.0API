5f34cbe039eca2d91f495d2292e1f7d6488e4ac9
elinasamuelsson <elina.samuelsson@live.com>
2026-01-21 15:37:03 +0100

    Create Cinema.java
    Klar tillsvidare, behöver Theater för att vara 100% klar.

---
+++ src/main/java/com/grupp5/sf2api/models/Cinema.java
@@ -0,0 +1,40 @@
+package com.grupp5.sf2api.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+/*
+ * Cinema behöver koppling till Theaters för att fungera.
+ * */
+@Entity
+@Table(name = "cinemas")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cinema {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String location;
+
+    @OneToMany(mappedBy = "cinemas",
+            cascade = CascadeType.ALL)
+    private List<Theater> theaters;
+
+    //Constructors
+    public Cinema(String name, String location) {
+        this.name = name;
+        this.location = location;
+    }
+}
