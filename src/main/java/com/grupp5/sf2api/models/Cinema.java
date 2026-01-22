cb0e775d818836275a272221d62d78a946c0b84e
elinasamuelsson <elina.samuelsson@live.com>
2026-01-22 11:48:08 +0100

    Update Cinema.java
    Removed @OneToMany connection and List<Theater> theaters from Cinema model.
    
    Removed redundant comment.

--- src/main/java/com/grupp5/sf2api/models/Cinema.java
+++ src/main/java/com/grupp5/sf2api/models/Cinema.java
@@ -6,12 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.List;
 import java.util.UUID;
 
-/*
- * Cinema behöver koppling till Theaters för att fungera.
- * */
 @Entity
 @Table(name = "cinemas")
 @Getter
@@ -28,10 +24,6 @@ public class Cinema {
 
     @Column(nullable = false)
     private String location;
-
-    @OneToMany(mappedBy = "cinemas",
-            cascade = CascadeType.ALL)
-    private List<Theater> theaters;
 
     //Constructors
     public Cinema(String name, String location) {
